39962c500c3d8933e776dc8d5191b1a1826b33fa
adwitkow <adamadik@poczta.fm>
2021-09-26 13:34:32 +0200

    Style: Remove unused usings

--- DSCSJsonEditor.WPF/MainWindow.xaml.cs
+++ DSCSJsonEditor.WPF/MainWindow.xaml.cs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-using System;
 using System.Windows;
 using DSCSJsonEditor.Core.Models;
 using DSCSJsonEditor.WPF.ViewModels;
