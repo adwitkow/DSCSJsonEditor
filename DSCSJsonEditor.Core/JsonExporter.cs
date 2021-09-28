4ca2840eaeaa9114b4ebec31935c66f035e27d2b
adwitkow <adamadik@poczta.fm>
2021-09-28 15:59:48 +0200

    When converting the JSON, flatten the filters to just strings

--- DSCSJsonEditor.Core/JsonExporter.cs
+++ DSCSJsonEditor.Core/JsonExporter.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,13 +19,36 @@ namespace DSCSJsonEditor.Core
                 NamingStrategy = new CamelCaseNamingStrategy(),
             };
 
-            return JsonConvert.SerializeObject(areas, new JsonSerializerSettings()
+            var serializer = new JsonSerializer()
             {
                 Formatting = Formatting.Indented,
                 NullValueHandling = NullValueHandling.Ignore,
                 ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
                 ContractResolver = contractResolver,
-            });
+            };
+
+            serializer.Converters.Add(new FilterConverter());
+
+            using (var textWriter = new StringWriter())
+            {
+                serializer.Serialize(textWriter, areas);
+                return textWriter.ToString();
+            }
+        }
+
+        private class FilterConverter : JsonConverter<Filter>
+        {
+            public override void WriteJson(JsonWriter writer, Filter value, JsonSerializer serializer)
+            {
+                writer.WriteValue(value.Name);
+            }
+
+            public override Filter ReadJson(JsonReader reader, Type objectType, Filter existingValue, bool hasExistingValue, JsonSerializer serializer)
+            {
+                string s = (string)reader.Value;
+
+                return new Filter(s);
+            }
         }
     }
 }
