9fc33872a0c3341f170ee808fe5640c050b8840f
adwitkow <adamadik@poczta.fm>
2021-09-30 14:02:14 +0200

    Style: Remove unused usings, reorder usings, 'this.' prefixes, file headers

--- DSCSJsonEditor.WPF/ViewModels/EditStepViewModel.cs
+++ DSCSJsonEditor.WPF/ViewModels/EditStepViewModel.cs
@@ -1,12 +1,25 @@
-﻿using DSCSJsonEditor.Core;
-using DSCSJsonEditor.Core.Models;
-using DSCSJsonEditor.WPF.Events;
+﻿// This file is part of DSCSJsonEditor project <https://github.com/adwitkow/DSCSJsonEditor>
+// Copyright (C) 2021  Adam Witkowski <https://github.com/adwitkow/>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DSCSJsonEditor.Core;
+using DSCSJsonEditor.Core.Models;
+using DSCSJsonEditor.WPF.Events;
 
 namespace DSCSJsonEditor.WPF.ViewModels
 {
--- DSCSJsonEditor.WPF/Views/NavigationView.xaml.cs
+++ DSCSJsonEditor.WPF/Views/NavigationView.xaml.cs
@@ -1,4 +1,20 @@
-﻿using System.Windows;
+﻿// This file is part of DSCSJsonEditor project <https://github.com/adwitkow/DSCSJsonEditor>
+// Copyright (C) 2021  Adam Witkowski <https://github.com/adwitkow/>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using System.Windows;
 using System.Windows.Controls;
 using DSCSJsonEditor.Core.Models;
 using DSCSJsonEditor.WPF.ViewModels;
@@ -6,13 +22,13 @@ using DSCSJsonEditor.WPF.ViewModels;
 namespace DSCSJsonEditor.WPF.Views
 {
     /// <summary>
-    /// Interaction logic for NavigationView.xaml
+    /// Interaction logic for NavigationView.xaml.
     /// </summary>
     public partial class NavigationView : UserControl
     {
         public NavigationView()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
 
         private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
